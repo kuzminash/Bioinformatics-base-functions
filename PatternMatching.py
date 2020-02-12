3c01510a0310bf79f8afb40f6d89d7089e9599c7
kuzminash <52103795+kuzminash@users.noreply.github.com>
2020-02-12 16:48:19 +0300

    Delete PatternMatching.py

--- PatternMatching.py
+++
@@ -1,8 +0,0 @@
-def PatternMatching(Pattern, Genome):
-    n = len(Genome)
-    k = len(Pattern)
-    positions = []
-    for position in range(n-k+1):
-        if Genome[position:position+k] == Pattern:
-            positions.append(position)
-    return positions
